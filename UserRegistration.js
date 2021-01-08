4fef5f98ee59c813aed41f63be28836bc1f34e7f
Rajas1312 <you@example.com>
2021-01-08 10:43:05 +0530

    Added: code to validate mobile number and refactored UC1 and UC2 to take user input

--- UserRegistration.js
+++ UserRegistration.js
@@ -2,6 +2,7 @@ console.log("welcome to user registration")
 const readLine=require('readline-sync')
 const name_pattern="^[A-Z]{1}[a-z]{2,}$";
 const email_pattern="^[a-zA-Z]{5}[0-9a-zA-Z\\.\\_\\-]*\\@[a-z]*\\.(com|co|in)$";
+const mobile_pattern="^(\\+91)?[6-9]{1}[0-9]{9}$";
 function checkFirstNameOrLastName(){
     let name=readLine.question("Enter a Name ")
     if(name.match(name_pattern)){
@@ -19,7 +20,17 @@ function checkEmailId(){
         console.log("valid email")
     }else{
         console.log("invalid email")
-       return checkEmailId()
+        checkEmailId()
     }
 }
 checkEmailId();
+function checkMobileNumber(){
+    let mobile=readLine.question("Enter a mobile number ")
+    if(mobile.match(mobile_pattern)){
+        console.log("valid mobile number")
+    }else{
+        console.log("invalid mobile number")
+        checkMobileNumber()
+    }
+}
+checkMobileNumber();
