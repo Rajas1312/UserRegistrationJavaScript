956f3a1e1a74771dba35387447b68dc8bda95b16
Rajas1312 <you@example.com>
2021-01-08 15:07:18 +0530

    refactored:added ES6 features and created class

--- UserRegistration.js
+++ UserRegistration.js
@@ -5,44 +5,45 @@ const email_pattern="^[a-zA-Z]{5}[0-9a-zA-Z\\.\\_\\-]*\\@[a-z]*\\.(com|co|in)$";
 const mobile_pattern="^(\\+91)?[6-9]{1}[0-9]{9}$";
 const password_pattern="^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9\\W]{8,}$";
 
-function checkFirstNameOrLastName(){
-    let name=readLine.question("Enter a Name ")
-    if(name.match(name_pattern)){
-        console.log("Valid Name")
-    }else{
-        console.log("Invaid Name")
-        checkFirstNameOrLastName()
+class UserRegistration{
+
+    checkFirstNameOrLastName=()=>{
+        let name=readLine.question("Enter a Name ")
+        if(name.match(name_pattern)){
+            console.log("Valid Name")
+        }else{
+            console.log("Invaid Name")
+            checkFirstNameOrLastName()
+        }
+    }
+    checkEmailId=()=>{
+        let email=readLine.question("Enter a email Id ")
+        if(email.match(email_pattern)){
+            console.log("valid email")
+        }else{
+            console.log("invalid email")
+            checkEmailId()
+        }
+    }
+     checkMobileNumber=()=>{
+        let mobile=readLine.question("Enter a mobile number ")
+        if(mobile.match(mobile_pattern)){
+            console.log("valid mobile number")
+        }else{
+            console.log("invalid mobile number")
+            checkMobileNumber()
+        }
+    }
+    checkPassword=()=>{
+        let password=readLine.question("Enter a password ")
+        if(password.match(password_pattern)){
+            console.log("valid password")
+        }else{
+            console.log("invalid password")
+            checkPassword()
+        }
     }
 }
-checkFirstNameOrLastName();
-checkFirstNameOrLastName();
-function checkEmailId(){
-    let email=readLine.question("Enter a email Id ")
-    if(email.match(email_pattern)){
-        console.log("valid email")
-    }else{
-        console.log("invalid email")
-        checkEmailId()
-    }
-}
-checkEmailId();
-function checkMobileNumber(){
-    let mobile=readLine.question("Enter a mobile number ")
-    if(mobile.match(mobile_pattern)){
-        console.log("valid mobile number")
-    }else{
-        console.log("invalid mobile number")
-        checkMobileNumber()
-    }
-}
-checkMobileNumber();
-function checkPassword(){
-    let password=readLine.question("Enter a password ")
-    if(password.match(password_pattern)){
-        console.log("valid password")
-    }else{
-        console.log("invalid password")
-        checkPassword()
-    }
-}
-checkPassword();
+userRegistration=new UserRegistration();
+module.exports=new UserRegistration();
+
--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-const utility=require('./utility')
-const readLine=require('readline-sync')
+const UserRegistration = require('./UserRegistration');
+const userRegistration=require('./UserRegistration')
+userRegistration.checkFirstNameOrLastName();
+userRegistration.checkFirstNameOrLastName();
+userRegistration.checkEmailId();
+userRegistration.checkMobileNumber();
+userRegistration.checkPassword();
 
-let firstName=readLine.question('Enter first name');
-const retData=utility.printName(firstName);
-console.log("it is in the main js",retData);
