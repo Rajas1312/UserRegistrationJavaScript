1aef3018c09ed03067ab47bbe70e7a3690970542
Rajas1312 <you@example.com>
2021-01-08 09:32:21 +0530

    Added:pattern to vaidate firstname

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,1 +1,10 @@
 console.log("welcome to user registration")
+let name_pattern="^[A-Z]{1}[a-z]{2,}$";
+function checkFirstName(firstName){
+    if(firstName.match(name_pattern)){
+        console.log("valid firstname")
+    }else{
+        console.log("invaid firstname")
+    }
+}
+checkFirstName("Rajas");
