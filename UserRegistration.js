dd47a9ec5d403f0ef11885fc86476e0a779e5c4e
Rajas1312 <you@example.com>
2021-01-08 10:11:51 +0530

    Added: code to validate email id and refactored UC1 and UC2 to take user input

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,11 +1,25 @@
 console.log("welcome to user registration")
-let name_pattern="^[A-Z]{1}[a-z]{2,}$";
-function checkFirstNameOrLastName(name){
+const readLine=require('readline-sync')
+const name_pattern="^[A-Z]{1}[a-z]{2,}$";
+const email_pattern="^[a-zA-Z]{5}[0-9a-zA-Z\\.\\_\\-]*\\@[a-z]*\\.(com|co|in)$";
+function checkFirstNameOrLastName(){
+    let name=readLine.question("Enter a Name ")
     if(name.match(name_pattern)){
         console.log("Valid Name")
     }else{
         console.log("Invaid Name")
+        checkFirstNameOrLastName()
     }
 }
-checkFirstNameOrLastName("Rajas");
-checkFirstNameOrLastName("Dongre");
+checkFirstNameOrLastName();
+checkFirstNameOrLastName();
+function checkEmailId(){
+    let email=readLine.question("Enter a email Id ")
+    if(email.match(email_pattern)){
+        console.log("valid email")
+    }else{
+        console.log("invalid email")
+       return checkEmailId()
+    }
+}
+checkEmailId();
