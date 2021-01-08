a729d76420e656b84e9658b8ca3393e3e3b2461a
Rajas1312 <you@example.com>
2021-01-08 09:38:49 +0530

    Added:pattern to vaidate last name

--- UserRegistration.js
+++ UserRegistration.js
@@ -1,10 +1,11 @@
 console.log("welcome to user registration")
 let name_pattern="^[A-Z]{1}[a-z]{2,}$";
-function checkFirstName(firstName){
-    if(firstName.match(name_pattern)){
-        console.log("valid firstname")
+function checkFirstNameOrLastName(name){
+    if(name.match(name_pattern)){
+        console.log("Valid Name")
     }else{
-        console.log("invaid firstname")
+        console.log("Invaid Name")
     }
 }
-checkFirstName("Rajas");
+checkFirstNameOrLastName("Rajas");
+checkFirstNameOrLastName("Dongre");
